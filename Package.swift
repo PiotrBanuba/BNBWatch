6cac588e319ad0d9411f0ce72d580eb96d2ec12b
Piotr Kulbaka <petr.kulbaka@banuba.com>
2023-10-16 12:18:34 +0300

    1.8.0-17

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.8.0-16"
+let bnbPackageVersion: Version = "1.8.0-17"
 
 let package = Package(
     name: "BNBWatch",
@@ -11,6 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "BNBWatch",
+            type: .dynamic,
             targets: [
                 "BNBWatch",
                 "BNBWatch_BNBSdkCore",
